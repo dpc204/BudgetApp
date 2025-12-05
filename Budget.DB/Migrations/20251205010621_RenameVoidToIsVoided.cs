cf88ef0a632d741a7bbbcee7a617abee122ee8a9
Patrick <dpc204@gmail.com>
2025-12-04 23:06:51 -0500

    Rename Void to IsVoided in Transaction entity
    Added a migration file `20251205010621_RenameVoidToIsVoided.cs`
    to rename the `Void` property to `IsVoided` in the `Transaction`
    entity. The migration file currently has empty `Up` and `Down`
    methods, indicating no schema changes have been implemented yet.
    
    Added the corresponding designer file
    `20251205010621_RenameVoidToIsVoided.Designer.cs`, which reflects
    the updated schema. This includes the `IsVoided` property in the
    `Transaction` entity, entity definitions, relationships, schema
    configurations, and seed data for various tables.

---
+++ Budget.DB/Migrations/20251205010621_RenameVoidToIsVoided.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Budget.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameVoidToIsVoided : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
