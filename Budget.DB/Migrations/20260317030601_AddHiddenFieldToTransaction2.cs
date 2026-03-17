b7866ddb00ce67c955828361314bd7985fc9b3ee
Patrick <dpc204@gmail.com>
2026-03-16 23:13:33 -0400

    Add TransactionHiddenFromAssign to TransactionDetails
    Introduced a new non-nullable boolean column, TransactionHiddenFromAssign, to the TransactionDetails table with a default value of false. Updated EF Core migration, model snapshot, and designer files to reflect this schema change.

---
+++ Budget.DB/Migrations/20260317030601_AddHiddenFieldToTransaction2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Budget.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHiddenFieldToTransaction2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "TransactionHiddenFromAssign",
+                schema: "budget",
+                table: "TransactionDetails",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionHiddenFromAssign",
+                schema: "budget",
+                table: "TransactionDetails");
+        }
+    }
+}
